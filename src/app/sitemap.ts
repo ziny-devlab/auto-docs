61cc369e4fd429f9d003731d83e9fbcff0a36e47
zinny22 <hziny722@naver.com>
2025-08-01 17:26:23 +0900

    fix: 사이트맵 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,17 +9,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
-    {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/blog`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
   ]
 }
